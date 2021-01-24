d23e2bbcda5bd5a662b87b56902afddfb8c866a1
pathnirvana <pathnirvana@gmail.com>
2021-01-24 16:24:16 +0700

    setup script

---
+++ setup_tacotron.sh
@@ -0,0 +1,8 @@
+cd tacotron2/
+git submodule init; 
+git submodule update
+
+git clone https://github.com/nvidia/apex
+pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./apex
+
+pip install -r tacotron2/requirements.txt
